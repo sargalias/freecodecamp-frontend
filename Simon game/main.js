4445253873e2ee425ea9617bee50d7d959b5c8a9
Spyros Argalias <sargalias@gmail.com>
2017-09-14 20:20:38 +0100

    working on simon game

---
+++ Simon game/main.js
@@ -0,0 +1,11 @@
+const LT = document.getElementById('lt');
+const RT = document.getElementById('rt');
+const LB = document.getElementById('lb');
+const RB = document.getElementById('lb');
+
+const START = document.getElementById('start');
+const STRICT = document.getElementById('strict');
+const DISPLAY = document.getElementById('display');
+
+const classNoInteract = "no-interact";
+const classStrictOn = "strict-on";
